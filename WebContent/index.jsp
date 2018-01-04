c4fde90398ef21d837a3269e4bf1b721d216a679
758770768 <758770768@qq.com>
2018-01-04 21:52:24 +0800

    springmvc新增测试

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,14 +1,34 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- jquery引入 -->
+<script type="text/javascript" src="js/jquery-3.0.0.js"></script>
 <title>Insert title here</title>
 </head>
 <body>
-	<jsp:forward page="testthankyou/hello"></jsp:forward>
+	<!-- 测试restful风格 -->
+	<%-- <jsp:forward page="testthankyou/hello"></jsp:forward> --%>
+	<!-- 测试@modelandAttribute注解 -->
+	<%-- <jsp:forward page="modelAttribute?uname=oliver&upass=123&bdate=2017-12-23"></jsp:forward> --%>
+	<!-- 测试自定义propertyeditorConverter -->
+	<%-- <jsp:forward page="propertyConverter?person=emma:male:2017-12-23"></jsp:forward> --%>
+	<!-- 测试自定义转换器 String2PersonConverter -->
+	<%-- <jsp:forward
+		page="string2PersonConverter?person=enna/female/2017-12-11"></jsp:forward> --%>
+	<span style="color: red">${param.result} <img alt="点击" src="" />
+	</span>
+	<form action="multipartResolver" method="post"
+		enctype="multipart/form-data">
+		<input type="file" name="mFile" /> <input type="submit" value="上传">
+	</form>
 	<script type="text/javascript">
+		$(function() {
+			$("img").prop("src", "/image/${param.path}");
+		});
 	</script>
+
 </body>
 </html>
--- src/org/test/controller/TestController.java
+++ src/org/test/controller/TestController.java
@@ -1,16 +1,32 @@
 package org.test.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.UsesSunHttpServer;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.test.converter.StringToPerson;
+import org.test.pojo.Person;
 import org.test.pojo.User;
 
 @Controller
+@SessionAttributes("user")
 public class TestController {
 
 	@Value("${age}")
@@ -21,17 +37,22 @@ public class TestController {
 	@Value("${height}")
 	private Integer height;
 
+	@ModelAttribute("user")
+	public User getUser() {
+		return new User();
+	}
+
 	@RequestMapping("/test{name}/{id}")
-	//cookie跨域请求
-	public String test(@PathVariable("name") String name, @PathVariable("id") String id,HttpServletResponse response) {
+	// cookie跨域请求
+	public String test(@PathVariable("name") String name, @PathVariable("id") String id, HttpServletResponse response) {
 		System.out.println(name + id);
 		System.out.println("age:" + age + "width:" + width);
-		Cookie cookie = new Cookie("user","name");
+		Cookie cookie = new Cookie("user", "name");
 		cookie.setPath("/");
 		cookie.setDomain("localhost");
-	    response.addCookie(cookie);	
+		response.addCookie(cookie);
 		return "test";
-		
+
 	}
 
 	@RequestMapping("/jsonDemo")
@@ -44,6 +65,60 @@ public class TestController {
 	@RequestMapping("/errorDemo")
 	public void fun3() {
 		int i = 3 / 0;
+	}
+
+	@RequestMapping("/modelAttribute")
+	public String fun4(@ModelAttribute("user") User user) {
+		return "a";
+	}
+
+	@RequestMapping("/sessionTest")
+	public String fun5() {
+		return "s";
+	}
+
+	/**
+	 * @param binder
+	 *            StringToPerson.3.注册自定义类型转换器
+	 */
+	/*
+	 * @InitBinder public void init(WebDataBinder binder) {
+	 * binder.registerCustomEditor(Person.class, new StringToPerson()); }
+	 */
+
+	/**
+	 * StringToPerson.4测试类型转换
+	 */
+	@RequestMapping("/propertyConverter")
+	public void fn1(@RequestParam(value = "person", required = false) Person person) {
+		System.out.println(person);
+	}
+
+	/**
+	 * String2PersonConverter.3测试类型转换
+	 */
+	@RequestMapping("/string2PersonConverter")
+	public void fn2(@RequestParam(value = "person", required = false) Person person) {
+		System.out.println(person);
+	}
+
+	/**
+	 * multipartResolver.2文件上传测试
+	 * 
+	 * @throws IOException
+	 */
+	@RequestMapping("/multipartResolver")
+	public String fn2(MultipartFile mFile, HttpServletRequest request, Map<String, String> map) throws IOException {
+		String oFileName = mFile.getOriginalFilename();
+		String type = oFileName.substring(oFileName.lastIndexOf("."));
+		String fileName = System.currentTimeMillis() + "." + type;
+		String path = "D:/apache-tomcat-7.0.81-windows-x86/apache-tomcat-7.0.81/webapps/image";
+		File file = new File(path, fileName);
+		// 流到文件的复制
+		FileUtils.copyInputStreamToFile(mFile.getInputStream(), file);
+		map.put("result", "successful" + file.getName());
+		map.put("path", file.getName());
+		return "redirect:/index.jsp";
 	}
 
 }
